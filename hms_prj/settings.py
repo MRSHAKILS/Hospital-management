a536003d4792a15950648f6834261632d3d1b8ef
Shakil <121shakilahmed@gmail.com>
2025-07-25 19:53:17 +0600

    time-zone fixed

--- hms_prj/settings.py
+++ hms_prj/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'hms_prj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR)],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
