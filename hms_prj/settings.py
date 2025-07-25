bf8d08cd073971dba9a0f55c0946f26c906627b5
Shakil <121shakilahmed@gmail.com>
2025-07-25 20:46:19 +0600

    .

--- hms_prj/settings.py
+++ hms_prj/settings.py
@@ -122,6 +122,17 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+LOGIN_URL = '/'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
