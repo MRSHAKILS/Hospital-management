7f32b10f542b59df92b175990c87e857270febc6
Shakil <121shakilahmed@gmail.com>
2025-07-28 17:57:41 +0600

    doctor model created

--- doctor/models.py
+++ doctor/models.py
@@ -1,2 +1,24 @@
 from django.db import models
-from userauths import models
+from django.utils import timezone
+
+
+from userauths import models as userauths_models
+
+
+class Doctor(models.Model):
+    user = models.OneToOneField(userauths_models.User, on_delete=models.CASCADE)
+    image = models.FileField(upload_to="images", null=True, blank=True)
+    full_name = models.CharField(max_length=100, null=True, blank=True)
+    mobile = models.CharField(max_length=100, null=True, blank=True)
+    country = models.CharField(max_length=100, null=True, blank=True)
+    bio = models.CharField(max_length=100, null=True, blank=True)
+    specialization = models.CharField(max_length=100, null=True, blank=True)
+    qualification = models.CharField(max_length=100, null=True, blank=True)
+    years_of_experience = models.CharField(max_length=100, null=True, blank=True)
+    next_available_appointment = models.CharField(max_length=100, null=True, blank=True)
+   
+
+    def __str__(self):
+        return f"Dr. {self.full_name}"
+
+
