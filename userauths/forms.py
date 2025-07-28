b32399df4769777885db19599e9a25b208e66fa5
Shakil <121shakilahmed@gmail.com>
2025-07-29 04:20:22 +0600

    .

---
+++ userauths/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
+
+class UserRegistrationForm(UserCreationForm):
+    full_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'John Doe'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'John Doe'}))
