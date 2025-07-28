8b0abd0144511fee373515c3945517ef86e9e66f
Shakil <121shakilahmed@gmail.com>
2025-07-29 03:06:27 +0600

    .

--- doctor/admin.py
+++ doctor/admin.py
@@ -1,16 +1,16 @@
-from django.contrib import admin
-from doctor import models
+# from django.contrib import admin
+# from doctor import models
 
 
 
-class DoctorAdmin(admin.ModelAdmin):
-    list_display = ['user', 'full_name', 'specialization', 'qualifications', 'years_of_experience']
+# class DoctorAdmin(admin.ModelAdmin):
+#     list_display = ['user', 'full_name', 'specialization', 'qualifications', 'years_of_experience']
 
 
-class NotificationAdmin(admin.ModelAdmin):
-    list_display = ['doctor', 'appointment', 'type', 'seen', 'date']
+# class NotificationAdmin(admin.ModelAdmin):
+#     list_display = ['doctor', 'appointment', 'type', 'seen', 'date']
 
 
 
-admin.site.register(models.Doctor, DoctorAdmin)
-admin.site.register(models.Notification, NotificationAdmin)
+# admin.site.register(models.Doctor, DoctorAdmin)
+# admin.site.register(models.Notification, NotificationAdmin)
