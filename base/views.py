c132fe240573d515c3bf359ea4ff54fcc07dc90a
Saleh6233 <salehsazzad6233@gmail.com>
2025-07-30 05:06:08 +0600

    Added authentication

--- base/views.py
+++ base/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from base import models as base_models
+from doctor import models as doctor_models
+from patient import models as patient_models
+
+
+def index(request):
+    services = base_models.Service.objects.all()
+    context = {
+        "services": services
+    }
+    return render(request, "base/index.html", context)
+
+
+def service_detail(request, service_id):
+    service = base_models.Service.objects.get(id=service_id)
+    context = {
+        "service": service
+    }
+    return render(request, "base/service_detail.html", context)
