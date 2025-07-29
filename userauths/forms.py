4b74cafd0062957571291d0a21f3f2121c200648
Shakil <121shakilahmed@gmail.com>
2025-07-30 02:13:48 +0600

    .

--- userauths/forms.py
+++ userauths/forms.py
@@ -4,4 +4,23 @@ from .models import User
 
 class UserRegisterForm(UserCreationForm):
     full_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'John Doe'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'John Doe'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'johndoe@gmail.com'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': '**************'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': '**************'}))
+
+
+    class Meta:
+        model = User
+        fields = ['full_name', 'email', 'password1', 'password2']
+
+
+
+
+class LoginForm(forms.Form):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'johndoe@gmail.com'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': '**************'}))
+
+
+    class Meta:
+        model = User
+        fields = ['email', 'password']
--- userauths/views.py
+++ userauths/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import messages
+from django.contrib import messages
+from userauths import forms as userauth_forms
 
 def register_view(request):
     if request.user.is_authenticated:
         messages.success(request, "You are already logged in.")
         return redirect('/')
 
-    form = None
+    form = userauth_forms.UserRegisterForm()
+    context = {
+        "form": form
+    }
+
+    return render(request, 'userauths/signup.html', context)
