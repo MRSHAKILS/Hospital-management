b1224eae9b1b42d5f6c51f6cd1c6cc7400cdf5a8
Shakil <121shakilahmed@gmail.com>
2025-07-30 07:00:01 +0600

    sign/up/out done

--- hms_prj/urls.py
+++ hms_prj/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('userauths.urls'))
+    path('', include('base.urls')),
+    path('auth/', include('userauths.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
