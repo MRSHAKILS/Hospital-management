0d88eca5e7d59b20f0b81ad5072ecdda35ff3a18
Saleh6233 <salehsazzad6233@gmail.com>
2025-08-01 18:43:12 +0600

    add appointment booking functionality

--- base/views.py
+++ base/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import render
-
-# Create your views here.
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from base import models as base_models
 from doctor import models as doctor_models
 from patient import models as patient_models
@@ -21,3 +19,70 @@ def service_detail(request, service_id):
         "service": service
     }
     return render(request, "base/service_detail.html", context)
+
+
+@login_required
+def book_appointment(request, service_id, doctor_id):
+    service = base_models.Service.objects.get(id=service_id)
+    doctor = doctor_models.Doctor.objects.get(id=doctor_id)
+    patient = patient_models.Patient.objects.get(user=request.user)
+
+    if request.method == "POST":
+        full_name = request.POST.get("full_name")
+        email = request.POST.get("email")
+        mobile = request.POST.get("mobile")
+        gender = request.POST.get("gender")
+        address = request.POST.get("address")
+        dob = request.POST.get("dob")
+        issues = request.POST.get("issues")
+        symptoms = request.POST.get("symptoms")
+
+        # Update patient bio data
+        patient.full_name = full_name
+        patient.email = email
+        patient.mobile = mobile
+        patient.gender = gender
+        patient.address = address
+        patient.dob = dob
+        patient.save()
+
+        # Create appointment object
+        appointment = base_models.Appointment.objects.create(
+            service=service,
+            doctor=doctor,
+            patient=patient,
+            appointment_date=doctor.next_available_appointment_date,
+            issues=issues,
+            symptoms=symptoms,
+        )
+
+        # Create a billing objects
+        billing = base_models.Billing()
+        billing.patient = patient
+        billing.appointment = appointment
+        billing.sub_total = appointment.service.cost
+        billing.tax = appointment.service.cost * 5 / 100
+        billing.total = billing.sub_total + billing.tax
+        billing.status = "Unpaid"
+        billing.save()
+
+        return redirect("base:checkout", billing.billing_id)
+
+    context = {
+        "service": service,
+        "doctor": doctor,
+        "patient": patient,
+    }
+    return render(request, "base/book_appointment.html", context)
+
+
+@login_required
+def checkout(request, billing_id):
+    billing = base_models.Billing.objects.get(billing_id=billing_id)
+
+    context = {
+        "billing": billing,
+        # "stripe_public_key": settings.STRIPE_PUBLIC_KEY,
+        # "paypal_client_id": settings.PAYPAL_CLIENT_ID,
+    }
+    return render(request, "base/checkout.html", context)
