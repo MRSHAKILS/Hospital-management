692425d2e7f968de16f674ce221cb7ba23ac2c5b
Shakil <121shakilahmed@gmail.com>
2025-08-05 02:28:18 +0600

    service created

--- base/views.py
+++ base/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 
+from base import models as base_models
+
 def index(request):
-    
-        return render(request, "base/index.html")
-   
+        services = base_models.Service.objects.all()
+        context = {
+            "services": services
+        }
+        return render(request, "base/index.html", context)
+
 
 def service_detail(request, service_id):
-        return render(request, "base/service-detail.html")
+        service = base_models.Service.objects.get(id=service_id)
+        context = {
+            "service": service
+        }
+        return render(request, "base/service-detail.html", context)
